f7086cfac510d74d7ea60da5b6cc79a7e56f762f
Theo Blauberg <tblauberg@gmail.com>
2024-07-27 10:31:48 +0300

    Update for skip button and leaderboard

--- app.R
+++ app.R
@@ -3,6 +3,8 @@ library(bslib)
 library(here)
 library(googlesheets4)
 library(tidyverse)
+library(shinyWidgets)
+library(DT)
 
 source("get_games.R")
 source("functions.R")
@@ -52,23 +54,43 @@ ui <- page_sidebar(
   theme = bs_theme(bootswatch = "minty"),
   sidebar = sidebar,
   card(
+    tableOutput("selects"),
+    max_height = 800,
 
-    tableOutput("selects")
   ),
   card(
     card_body(
-      textOutput("players"),
-      h2("Syötä pelin tulokset: "),
-      fluidRow(
-        point_input("player_1_points", "Pelaaja 1"),
-        point_input("player_2_points", "Pelaaja 2")
+      h2(textOutput("player_header")),
+      sliderTextInput(
+        inputId = "game_result",
+        label = NULL,
+        choices = c(
+          "3-0",
+          "2-0",
+          "1-0",
+          "0-0",
+          "0-1",
+          "0-2",
+          "0-3"
+        ),
+        selected = "0-0",
+        width = "100%"
       ),
       input_task_button(
         id = "save_points",
         label = "Tallenna pisteet",
         label_busy = "Tallennetaan..."
+      ),
+      input_task_button(
+        id = "skip_button",
+        label = "Ohita vuoro",
+        label_busy = "Ohitetaan..."
       )
-    )
+    ),
+    min_height = 250
+  ),
+  card(
+    DT::DTOutput("leader_board")
   )
 )
 
@@ -80,6 +102,11 @@ server <- function(input, output, session) {
     new = get_new_games(),
     old = get_old_games()
     )
+
+  points <- reactiveValues(
+    player_1 = NA,
+    player_2 = NA
+  )
 
   observe({
     games$new <-  get_games(input$participants) %>%
@@ -134,22 +161,95 @@ server <- function(input, output, session) {
 
   })
 
+  output$player_header <-
+    renderText({
+
+      glue::glue(
+        "{play_turn()$player_1} - {play_turn()$player_2}"
+      ) %>%
+        as.character()
+
+    })
 
   observe({
-    updateNumericInput(
+    updateSliderTextInput(
       session = session,
-      inputId = "player_1_points",
-      value = 0,
-      label = play_turn()$player_1)
-
-    updateNumericInput(
-      session = session,
-      inputId = "player_2_points",
-      value = 0,
-      label = play_turn()$player_2)
-
+      inputId = "game_result",
+      label = NULL,
+      selected = "0-0"
+    )
     }) %>%
     bindEvent(play_turn())
+
+
+
+
+
+  observe({
+    values <- str_split(input$game_result, '-')
+
+    points$player_1 <- values[[1]][1]
+    points$player_2 <- values[[1]][2]
+
+  }) %>%
+    bindEvent(input$game_result)
+
+
+  leader_board <- reactive({
+
+    data <- games$new %>%
+      mutate(
+        player_1_win = if_else(player_1_point > player_2_point, 1, 0),
+        player_2_win = if_else(player_2_point > player_1_point, 1, 0)
+      )
+
+    data %>%
+      select(
+        player_1,
+        player_1_point,
+        player_2_point,
+        player_1_win,
+        player_2_win
+      ) %>%
+      rename(
+        player = player_1,
+        point = player_1_point,
+        lost_point = player_2_point,
+        win = player_1_win,
+        lost = player_2_win) %>%
+      bind_rows(
+        data %>%
+          select(
+            player_2,
+            player_2_point,
+            player_1_point,
+            player_2_win,
+            player_1_win
+          ) %>%
+          rename(
+            player = player_2,
+            point = player_2_point,
+            lost_point = player_1_point,
+            win = player_2_win,
+            lost = player_1_win
+            )
+        ) %>%
+      group_by(player) %>%
+      summarise_if(
+        is.numeric,
+        sum,
+        na.rm = T
+        ) %>%
+      arrange(desc(point))
+
+
+
+  })
+
+  output$leader_board <- DT::renderDT({
+    datatable(leader_board())
+
+  })
 
   output$selects <- renderTable({
     games$new %>%
@@ -166,13 +266,24 @@ server <- function(input, output, session) {
       games$new$player_2 == play_turn()$player_2 &
       games$new$player_1 == play_turn()$player_1),
       c("player_1_point","player_2_point", "play_dttm")] <- list(
-        input$player_1_points %>% as.integer(),
-        input$player_2_points %>% as.integer(),
+        points$player_1 %>% as.integer(),
+        points$player_2 %>% as.integer(),
         Sys.time()
         )
   }) %>%
     bindEvent(input$save_points)
 
+
+
+
+
+  observe({
+    games$new[(
+      games$new$player_2 == play_turn()$player_2 &
+      games$new$player_1 == play_turn()$player_1),
+      c("order_num")] <- max(games$new$order_num) + 1
+    }) %>%
+    bindEvent(input$skip_button)
 
 }
 
